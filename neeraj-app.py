defb947a4022bb2d083e00f90a33b0b2c34f4040
neerajkumr <70322843+neerajkumr@users.noreply.github.com>
2020-08-27 19:15:16 +0530

    Update neeraj-app.py

--- neeraj-app.py
+++ neeraj-app.py
@@ -25,7 +25,7 @@ def follower_count(user):
   emoji_followers = ''.join([emoji_numbers[j] for j in uf_split if j in emoji_numbers.keys()]) 
   return emoji_followers
 
-api = create_api()
+api = create_api() 
 
 def main():
   while True:
